6651ba5c9d988ca1d88428f5de67d092829132ed
Apeami <kaiduncumb@icloud.com>
2023-08-08 10:59:03 +0100

    Added Train Drawing

--- src/train.py
+++ src/train.py
@@ -1,11 +1,15 @@
+import pyglet
 
+class Train:
+    def __init__(self,trainData,openGlInstance,shape,headcode):
+        self.openGlInstance = openGlInstance
+        self.shape = shape
+        self.headcode = headcode
 
-class train:
-    def __init__(self,trainData):
-        self.headcode = trainData['Headcode']
-        self.maxSpeed = trainData['MaxSpeed']
+        #self.headcode = trainData['Headcode']
+        #self.maxSpeed = trainData['MaxSpeed']
 
-        self.sorted_schedule = sorted(trainData["Schedule"], key=lambda x: x["Time"])
+        #self.sorted_schedule = sorted(trainData["Schedule"], key=lambda x: x["Time"])
 
         self.currentEvent = 0
 
@@ -14,3 +18,29 @@ class train:
 
     def updateMinorEvent(self):
         pass
+
+    def drawTrain(self,worldPos):
+
+        width, height = 40, 20
+        x = worldPos[0]+25 -(width/2)
+        y = worldPos[1]+25 -(height/2)
+
+        rectLayer = pyglet.graphics.OrderedGroup(2)
+        textLayer = pyglet.graphics.OrderedGroup(3)
+
+        # Draw text on the rectangle
+        label = pyglet.text.Label(self.headcode,
+            font_name='Arial',
+            font_size=12,
+            x=x + width/2, y=y + height/2,
+            anchor_x='center', anchor_y='center',
+            batch=self.openGlInstance.batch,
+            group = textLayer
+        )
+
+        rectangle_vertices = (x, y, x + width, y, x + width, y + height, x, y + height)
+        rectangle_colors = (255, 0, 0, 255, 0, 0, 255, 0, 0, 255, 0, 0)
+        self.openGlInstance.batch.add(4, pyglet.gl.GL_QUADS,rectLayer,
+              ('v2f', rectangle_vertices),
+              ('c3B', rectangle_colors))
+
